7251e716d33a6ad383d35b63bd105de2976b635e
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2021-10-07 15:27:57 +0200

    update connectivity_plot function

--- AstrocyteNeuron_Interactions/Brian2_tutorial/connectivity.py
+++ AstrocyteNeuron_Interactions/Brian2_tutorial/connectivity.py
@@ -5,19 +5,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from brian2 import *
 
-def Connectivity_plot(Syn):
+def Connectivity_plot(Syn, source='Source', target='Target', color_s='k', color_t='k', size=35):
     """
     Connectiovity of neuronal network
 
     Parameters
     ----------
-    Syn: 'brian2.synapses.synapses.Synapses'
+    Syn : 'brian2.synapses.synapses.Synapses'
         Synapses object when the connectivity is defined
 
+    source : string (optional)
+            name of Sourge network. Default='Source'
+
+    target : string (optional)
+            name of Target network. Default='Target'
+
+    color_s : array-like or list of colors or color (optional)
+        color of Source network. See matplotlib.color for more information about color.
+
+    color_s : array-like or list of colors or color (optional)
+        color of Target network. See matplotlib.color for more information about color.
+    
+    size : integer or float (optional)
+        marker size. Default=35
     Returns
     -------
-
     """
+
     Ns = len(Syn.source) #number of neuron in Source
     Nt = len(Syn.target) #number of neuron in Target
 
@@ -25,16 +39,16 @@ def Connectivity_plot(Syn):
     ax1 = fig.add_subplot(1,2,1)
     ax2 = fig.add_subplot(1,2,2)
 
-    ax1.plot(np.zeros(Ns), np.arange(Ns), 'ok', ms=10)
-    ax1.plot(np.ones(Nt), np.arange(Nt), 'ok', ms=10)
+    ax1.scatter(np.zeros(Ns), np.arange(Ns), marker='o',s=size, color=color_s) 
+    ax1.scatter(np.ones(Nt), np.arange(Nt), marker='o', s=size, color=color_t)
     for i,j in zip(Syn.i,Syn.j):
-        ax1.plot([0,1], [i,j], '-k')
+        ax1.plot([0,1], [i,j], lw=0.4, color=color_s)
     ax1.set_xticks([0,1])
-    ax1.set_xticklabels(['Source', 'Target'])
+    ax1.set_xticklabels([source, target])
     ax1.set_ylabel('Neuron index')
     ax1.set_title('Connectivity')
 
-    ax2.plot(Syn.i, Syn.j, 'ok')
+    ax2.scatter(Syn.i, Syn.j, marker='o', s=size, color='k')
     ax2.set_xlabel('Source neuron index')
     ax2.set_ylabel('Target neuron index')
     ax2.set_title('Source vs Target connectivity')
