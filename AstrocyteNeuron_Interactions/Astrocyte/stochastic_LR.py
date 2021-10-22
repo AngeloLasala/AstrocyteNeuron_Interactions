d3505548edc9e3685734d3eb2a9c06aa1c3f4417
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2021-10-22 11:43:07 +0200

    minor changes

--- AstrocyteNeuron_Interactions/Astrocyte/stochastic_LR.py
+++ AstrocyteNeuron_Interactions/Astrocyte/stochastic_LR.py
@@ -44,16 +44,12 @@ def LiRinzel(X, t, I):
 
     Q_2 = d_2*((I+d_1)/(I+d_3))                 
     m_inf = (I/(I+d_1))*(C/(C+d_5))              
-    tau_h = 1 / (O_2*(Q_2+C))                    
-    h_inf = Q_2/(Q_2+C)
-
     alpha =  O_2 * Q_2
     beta =  O_2 * C
 
     J_r = Omega_C*(m_inf**3)*(h**3)*(C_T-(1+rho_A)*C)  
     J_l = Omega_L*(C_T-(1+rho_A)*C)                    
     J_p = (O_P*C**2)/(K_P**2+C**2)                    
-
 
     dvdt = [J_r + J_l - J_p,
             alpha * (1-h) - beta * h]
@@ -94,17 +90,13 @@ def stochastic_LiRinzel(X, t, I, N):
     C,h = X
 
     Q_2 = d_2*((I+d_1)/(I+d_3))                 
-    m_inf = (I/(I+d_1))*(C/(C+d_5))              
-    tau_h = 1 / (O_2*(Q_2+C))                    
-    h_inf = Q_2/(Q_2+C)
-
+    m_inf = (I/(I+d_1))*(C/(C+d_5))
     alpha =  O_2 * Q_2
     beta =  O_2 * C
 
     J_r = Omega_C*(m_inf**3)*(h**3)*(C_T-(1+rho_A)*C)  
     J_l = Omega_L*(C_T-(1+rho_A)*C)                    
     J_p = (O_P*C**2)/(K_P**2+C**2)                    
-
 
     dvdt = [0,
             (alpha * (1-h) + beta * h) / N]
@@ -151,9 +143,114 @@ def der_stochastic_LiRinzel(X, t, I, N):
     J_l = Omega_L*(C_T-(1+rho_A)*C)                    
     J_p = (O_P*C**2)/(K_P**2+C**2)                    
 
-
     dvdt = [0,
             (-alpha + beta) / N]
+
+    return np.array(dvdt)
+
+def LiRinzel_multiplicative(X, t, I):
+    """
+    Li-Rinzel model with multiplicative noise, dynamical behavior of calcium oscillation
+    in a single astrocyte.
+.
+    Parameters
+    ----------
+    X : list, numpy array
+        array of dynamical variables
+
+    t : float
+        time variable
+
+    I : float
+        IP3 concentration
+
+    Returns
+    -------
+    dvdt: numpy.array
+        numpy array of vector field
+
+    """
+    C,h = X
+
+    Q_2 = d_2*((I+d_1)/(I+d_3))                 
+    m_inf = (I/(I+d_1))*(C/(C+d_5))              
+    tau_h = 1 / (O_2*(Q_2+C))                    
+    h_inf = Q_2/(Q_2+C)
+
+    J_r = Omega_C*(m_inf**3)*(h**3)*(C_T-(1+rho_A)*C)  
+    J_l = Omega_L*(C_T-(1+rho_A)*C)                    
+    J_p = (O_P*C**2)/(K_P**2+C**2)
+
+    dvdt = [J_r + J_l - J_p,
+            (h_inf - h) / tau_h]
+
+    return np.array(dvdt)
+
+def stochastic_LiRinzel_multiplicative(X, t, I):
+    """
+    Li-Rinzel model with multiplicative noise, dynamical behavior of calcium oscillation
+    in a single astrocyte.
+.
+    Parameters
+    ----------
+    X : list, numpy array
+        array of dynamical variables
+
+    t : float
+        time variable
+
+    I : float
+        IP3 concentration
+
+    Returns
+    -------
+    dvdt: numpy.array
+        numpy array of vector field
+
+    """
+    C,h = X
+
+    Q_2 = d_2*((I+d_1)/(I+d_3))                 
+    m_inf = (I/(I+d_1))*(C/(C+d_5))              
+    tau_h = 1 / (O_2*(Q_2+C))                    
+    h_inf = Q_2/(Q_2+C)
+                    
+    dvdt = [0,
+            (h_inf - h) / (math.sqrt(tau_h))]
+
+    return np.array(dvdt)
+
+def der_stochastic_LiRinzel_multiplicative(X, t, I):
+    """
+    Li-Rinzel model with multiplicative noise, dynamical behavior of calcium oscillation
+    in a single astrocyte.
+.
+    Parameters
+    ----------
+    X : list, numpy array
+        array of dynamical variables
+
+    t : float
+        time variable
+
+    I : float
+        IP3 concentration
+
+    Returns
+    -------
+    dvdt: numpy.array
+        numpy array of vector field
+
+    """
+    C,h = X
+
+    Q_2 = d_2*((I+d_1)/(I+d_3))                 
+    m_inf = (I/(I+d_1))*(C/(C+d_5))              
+    tau_h = 1 / (O_2*(Q_2+C))                    
+    h_inf = Q_2/(Q_2+C)
+                    
+    dvdt = [0,
+            -1/(math.sqrt(tau_h))]
 
     return np.array(dvdt)
 
@@ -176,7 +273,7 @@ if __name__ == "__main__":
     I = 0.4
 
     #fixed ramdom seed for reproducibility
-    np.random.seed(199586) 
+    # np.random.seed(199586) 
     
     #Deterministic solutions
     t0 = 0.      #sec
@@ -190,23 +287,42 @@ if __name__ == "__main__":
     C_exc = sol[:,0]
     h_exc = sol[:,1]
 
-    #Euler-Milstein stochastic
-    N_values = [5,10,100,1000]
+    #Euler-Milstein stochastic LR
+    N_values = [1,10,100,1000]
     sol = []
+    Y1_list = []
     for N in N_values:
         X0 =  np.array([0.2,0.2])
         sol_N = []
         for i in t:
-            Z1 = np.random.normal(0,1)
-            X_new = X0 + dt * LiRinzel(X0, i, I) + stochastic_LiRinzel(X0, i, I, N)*math.sqrt(dt)*Z1 + 0.5*der_stochastic_LiRinzel(X0, i, I, N)*dt*Z1**2
+            Y1 = np.random.normal(0,1)
+            Y1_list.append(Y1)
+            X_new = X0 + dt * LiRinzel(X0, i, I) + stochastic_LiRinzel(X0, i, I, N)*math.sqrt(dt)*Y1 
+            + 0.5*der_stochastic_LiRinzel(X0, i, I, N)*stochastic_LiRinzel(X0, i, I, N)*dt*Y1**2
             sol_N.append(X_new)
             X0 = X_new
         sol.append(sol_N)
 
     sol = np.array(sol)
+    
+    
+    #Euler-Milstein stochastic LR_multiplicative
+    X0 =  np.array([0.2,0.2])
+    sol_m = []
+    for en,i in enumerate(t):
+        Y1 = np.random.normal(0,1)
+        X_new = X0 + dt * LiRinzel_multiplicative(X0, i, I)+ stochastic_LiRinzel_multiplicative(X0, i, I)*math.sqrt(dt)*Y1 
+        + 0.5*der_stochastic_LiRinzel_multiplicative(X0, i, I)*stochastic_LiRinzel_multiplicative(X0, i, I)*dt*Y1**2
+        sol_m.append(X_new)
+        X0 = X_new
+    sol_m = np.array(sol_m)
+
+    print(sol_m.shape)
+
+    
 
     # Plots
-    fig1 = plt.figure(num='Stochastic Li Rinzel model', figsize=(10,10))
+    fig1 = plt.figure(num='Stochastic Li Rinzel model', figsize=(12,10))
     ax11 = fig1.add_subplot(2,1,1)
     ax12 = fig1.add_subplot(2,1,2)
 
@@ -224,5 +340,20 @@ if __name__ == "__main__":
     ax12.set_xlabel('time (s)')
     ax12.grid(linestyle='dotted')
     ax12.legend()
+
+    fig2 = plt.figure(num='Stochastic Li Rinzel model - multiplicative noise', figsize=(12,10))
+    ax21 = fig2.add_subplot(2,1,1)
+    ax22 = fig2.add_subplot(2,1,2)
+
+    ax21.plot(t, sol_m[:,0], label='C mult_noise')
+    ax21.set_ylabel(r'$Ca^{2\plus}$ ($\mu$M)')
+    ax21.grid(linestyle='dotted')
+    ax21.legend()
+
+    ax22.plot(t, sol_m[:,1], label='h mult_noise') 
+    ax22.set_ylabel('h')
+    ax22.set_xlabel('time (s)')
+    ax22.grid(linestyle='dotted')
+    ax22.legend()
 
     plt.show()
