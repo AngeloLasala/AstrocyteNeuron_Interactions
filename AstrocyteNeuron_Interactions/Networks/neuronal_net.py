547f0844424d8ad3d9e88280979f36445f92a769
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2021-11-05 08:56:11 +0100

    changed plot statements

--- AstrocyteNeuron_Interactions/Networks/neuronal_net.py
+++ AstrocyteNeuron_Interactions/Networks/neuronal_net.py
@@ -10,7 +10,7 @@ from brian2 import *
 from AstrocyteNeuron_Interactions.Brian2_utils.connectivity import connectivity_plot
 
 # Parameters
-N_e = 3600                #Total number of excitatory neurons
+N_e = 3200                #Total number of excitatory neurons
 N_i = 800                #Total number of inhibitory neurons
 duration = 1.0*second  # Total simulation time
 seed(19958)
@@ -24,7 +24,7 @@ C_m = 198*pF           # Membrane capacitance
 tau_e = 5*ms           # Excitatory synaptic time constant
 tau_i = 10*ms          # Inhibitory synaptic time constant
 tau_r = 5*ms           # Refractory period
-I_ex = 100*pA          # External current
+I_ex = 150*pA          # External current
 V_th = -50*mV          # Firing threshold
 V_r = E_l              # Reset potential 
 
@@ -107,46 +107,42 @@ print()
 print(syn_exc_mon.u_S)
 print('\n\n')
 
-#Plots
-fig1 = plt.figure(num=f'exc variable dynamics, Ne:{N_e} Ni:{N_i}, Iex={I_ex/pA}',figsize=(9,10))
-ax11 = fig1.add_subplot(3,1,1)
-ax12 = fig1.add_subplot(3,1,2)
-ax13 = fig1.add_subplot(3,1,3)
+# Plots  ################################################################################################
+fig1, ax1 = plt.subplots(nrows=3, ncols=1, sharex=True, 
+                         num=f'exc variable dynamics, Ne:{N_e} Ni:{N_i}, Iex={I_ex/pA}',figsize=(9,10))
 
 
-ax11.plot(state_exc_mon.t/ms, state_exc_mon.v[0]/mV, label=f'neuron {index}')
-ax11.axhline(V_th/mV, color='C2', linestyle=':')
+ax1[0].plot(state_exc_mon.t/ms, state_exc_mon.v[0]/mV, label=f'neuron {index}')
+ax1[0].axhline(V_th/mV, color='C2', linestyle=':')
 for spk in spikes_exc_mon.t[spikes_exc_mon.i == index]:
-    ax11.axvline(x=spk/ms, ymin=0.15, ymax=0.95 )
-ax11.set_ylim(bottom=-60.8, top=0.1)
-ax11.set_ylabel('Membran potential (V)')
-ax11.set_title('Neurons dynamics')
-ax11.grid(linestyle='dotted')
-ax11.legend(loc = 'upper right')
+    ax1[0].axvline(x=spk/ms, ymin=0.15, ymax=0.95 )
+ax1[0].set_ylim(bottom=-60.8, top=0.1)
+ax1[0].set_ylabel('Membran potential (V)')
+ax1[0].set_title('Neurons dynamics')
+ax1[0].grid(linestyle='dotted')
+ax1[0].legend(loc = 'upper right')
 
+ax1[1].plot(state_exc_mon.t/ms, state_exc_mon.g_i[0]/nS, color='C4', label=f'{index}'+r' $g_i$')
+ax1[1].plot(state_exc_mon.t/ms, state_exc_mon.g_e[0]/nS, color='C5', label=f'{index}'+r' $g_e$')
+ax1[1].set_ylabel('Conductance (nS)')
+ax1[1].grid(linestyle='dotted')
+ax1[1].legend(loc = 'upper right')
 
-ax12.plot(state_exc_mon.t/ms, state_exc_mon.g_i[0]/nS, color='C4', label=f'{index}'+r' $g_i$')
-ax12.plot(state_exc_mon.t/ms, state_exc_mon.g_e[0]/nS, color='C5', label=f'{index}'+r' $g_e$')
-ax12.set_ylabel('Conductance (nS)')
-ax12.grid(linestyle='dotted')
-ax12.legend(loc = 'upper right')
+ax1[2].plot(syn_exc_mon.t/ms, syn_exc_mon.u_S[0], label=f'{index}'+r' $u_S$', color='C1')
+ax1[2].plot(syn_exc_mon.t/ms, syn_exc_mon.x_S[0], label=f'{index}'+r' $x_S$', color='C4')
+ax1[2].set_xlabel('time (ms)')
+ax1[2].set_ylabel(r'$u_S$, $x_S$')
+ax1[2].grid(linestyle='dotted')
+ax1[2].legend(loc = 'upper right')
 
-ax13.plot(syn_exc_mon.t/ms, syn_exc_mon.u_S[0], label=f'{index}'+r' $u_S$', color='C1')
-ax13.plot(syn_exc_mon.t/ms, syn_exc_mon.x_S[0], label=f'{index}'+r' $x_S$', color='C4')
-ax12.set_xlabel('time (ms)')
-ax13.set_ylabel(r'$u_S$, $x_S$')
-ax13.grid(linestyle='dotted')
-ax13.legend(loc = 'upper right')
+fig2, ax2 = plt.subplots(nrows=1, ncols=1, 
+                         num=f'Raster plot, Ne:{N_e} Ni:{N_i}, Iex={I_ex/pA}', figsize=(8,8))
 
-
-fig2 = plt.figure(num=f'Raster plot, Ne:{N_e} Ni:{N_i}, Iex={I_ex/pA}', figsize=(8,8))
-ax21 = fig2.add_subplot(1,1,1)
-
-ax21.scatter(spikes_exc_mon.t[:], spikes_exc_mon.i[:], color='C3', marker='|')
-ax21.scatter(spikes_inh_mon.t[:], spikes_inh_mon.i[:]+N_e, color='C0', marker='|')
-ax21.set_xlabel('time (s)')
-ax21.set_ylabel('neuron index')
-ax21.set_title('Raster plot')
+ax2.scatter(spikes_exc_mon.t[:], spikes_exc_mon.i[:], color='C3', marker='|')
+ax2.scatter(spikes_inh_mon.t[:], spikes_inh_mon.i[:]+N_e, color='C0', marker='|')
+ax2.set_xlabel('time (s)')
+ax2.set_ylabel('neuron index')
+ax2.set_title('Raster plot')
 
 # Connectivity_plot(exc_syn, source='Exc', target='Exc+Inh', color_s='red', color_t='indigo', size=10)
 # Connectivity_plot(inh_syn, source='Inh', target='Exc+Inh', color_s='C0', color_t='indigo', size=10)
