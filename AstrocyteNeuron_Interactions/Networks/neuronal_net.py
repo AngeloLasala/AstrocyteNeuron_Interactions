9a17582f19642aa64a2addecfbb5468eb440d46e
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2021-12-18 18:06:16 +0100

    EI network with STP

--- AstrocyteNeuron_Interactions/Networks/neuronal_net.py
+++ AstrocyteNeuron_Interactions/Networks/neuronal_net.py
@@ -5,6 +5,7 @@ Randomly connected networks with conductance-based synapses.
 - "Modeling euron-glia interaction with Brian 2 simulator", Stimberg et al (2017)
 """
 import matplotlib.pyplot as plt
+from scipy.fft import fft, fftfreq
 from random import randrange
 from brian2 import *
 from AstrocyteNeuron_Interactions.Brian2_utils.connectivity import connectivity_EIring
@@ -12,7 +13,7 @@ from AstrocyteNeuron_Interactions.Brian2_utils.connectivity import connectivity_
 ## Parameters ########################################################################
 
 # Network size
-N_e = 3200                #Total number of excitatory neurons
+N_e = 3200               #Total number of excitatory neurons
 N_i = 800                #Total number of inhibitory neurons
 
 #Neurons parameters
@@ -24,7 +25,7 @@ C_m = 198*pF           # Membrane capacitance
 tau_e = 5*ms           # Excitatory synaptic time constant
 tau_i = 10*ms          # Inhibitory synaptic time constant
 tau_r = 5*ms           # Refractory period
-I_ex = 150*pA          # External current
+I_ex = 110*pA          # External current
 V_th = -50*mV          # Firing threshold
 V_r = E_l              # Reset potential 
 
@@ -37,7 +38,7 @@ Omega_f = 3.33/second  # Synaptic facilitation rate
 #############################################################################################
 
 ## MODEL   ##################################################################################
-
+defaultclock.dt = 0.1*ms
 duration = 1.0*second  # Total simulation time
 seed(19958)
 
@@ -46,9 +47,8 @@ neuron_eqs = """
 dv/dt = (g_l*(E_l-v) + g_e*(E_e-v) + g_i*(E_i-v) + I_ex)/C_m : volt (unless refractory)
 dg_e/dt = -g_e/tau_e : siemens
 dg_i/dt = -g_i/tau_i : siemens
+LFP = (abs(g_e*(E_e-v)) + abs(g_i*(E_i-v))) : ampere
 """
-Np = 0
-poisson = PoissonGroup(Np, rates=500.0*Hz)
 neurons = NeuronGroup(N_e+N_i, model=neuron_eqs, method='rk4',
                      threshold='v>V_th', reset='v=V_r', refractory='tau_r')
 
@@ -85,34 +85,47 @@ inh_syn.x_S = 1
 ## RUN and MONITOR  ######################################################################### 
 spikes_exc_mon = SpikeMonitor(exc_neurons)
 spikes_inh_mon = SpikeMonitor(inh_neurons)
+firing_rate_exc = PopulationRateMonitor(exc_neurons)
+firing_rate_ihn = PopulationRateMonitor(inh_neurons)
 
 # select random excitatory neurons
-index = randrange(N_e) 
-
-state_exc_mon = StateMonitor(exc_neurons, ['v', 'g_e', 'g_i'], record=index)
+index = 362
+state_exc_mon = StateMonitor(exc_neurons, ['v', 'g_e', 'g_i', 'LFP'], record=True)
 syn_exc_mon = StateMonitor(exc_syn, ['u_S','x_S'], record=exc_syn[index, :]) 
 syn_inh_mon = StateMonitor(inh_syn, ['u_S','x_S'], record=inh_syn[index, :])
 #record=exc_syn[index, :], outgoing synapses from neurons labeled by index
-spikes_mon = SpikeMonitor(neurons)
+
 
 run(duration, report='text')
+print('NETWORK')
+print(f'exc syn: {exc_syn.N[:]}')
+print(f'inh syn: {inh_syn.N[:]}')
+print()
 print(f'exc neuron number: {index}')
-print(f'exc syn: {len(syn_exc_mon.u_S[:])}')
-print(f'inh syn: {len(syn_inh_mon.u_S[:])}')
-print()
-print(exc_syn[index, :])
-print(inh_syn[index, :])
-print()
-print(syn_exc_mon.u_S)
-print('\n\n')
+print(f'exc syn: {syn_exc_mon.u_S[:].shape}')
+print('\n')
+
+## Network variable
+LFP = state_exc_mon.LFP[:].sum(axis=0)
+fr_exc = firing_rate_exc.smooth_rate(window='gaussian', width=2*ms)
+fr_ihn = firing_rate_ihn.smooth_rate(window='gaussian', width=2*ms)
+
+fr_exc_fft = fft(fr_exc)
+N = len(fr_exc)
+fr_exc_freq = fftfreq(N,defaultclock.dt)
+
+plt.figure()
+plt.plot(fr_exc_freq[:N//2], np.abs(fr_exc_fft[:N//2]**2))
+
+
+
 #########################################################################################################
 
 # Plots  ################################################################################################
 fig1, ax1 = plt.subplots(nrows=3, ncols=1, sharex=True, 
-                         num=f'exc variable dynamics, Ne:{N_e} Ni:{N_i}, Iex={I_ex/pA}',figsize=(9,10))
+                         num=f'exc variable dynamic, I_ex={I_ex/pA}',figsize=(9,10))
 
-
-ax1[0].plot(state_exc_mon.t/ms, state_exc_mon.v[0]/mV, label=f'neuron {index}')
+ax1[0].plot(state_exc_mon.t/ms, state_exc_mon.v[index,:]/mV, label=f'neuron {index}')
 ax1[0].axhline(V_th/mV, color='C2', linestyle=':')
 for spk in spikes_exc_mon.t[spikes_exc_mon.i == index]:
     ax1[0].axvline(x=spk/ms, ymin=0.15, ymax=0.95 )
@@ -122,8 +135,8 @@ ax1[0].set_title('Neurons dynamics')
 ax1[0].grid(linestyle='dotted')
 ax1[0].legend(loc = 'upper right')
 
-ax1[1].plot(state_exc_mon.t/ms, state_exc_mon.g_i[0]/nS, color='C4', label=f'{index}'+r' $g_i$')
-ax1[1].plot(state_exc_mon.t/ms, state_exc_mon.g_e[0]/nS, color='C5', label=f'{index}'+r' $g_e$')
+ax1[1].plot(state_exc_mon.t/ms, state_exc_mon.g_i[index,:]/nS, color='C4', label=f'{index}'+r' $g_i$')
+ax1[1].plot(state_exc_mon.t/ms, state_exc_mon.g_e[index,:]/nS, color='C5', label=f'{index}'+r' $g_e$')
 ax1[1].set_ylabel('Conductance (nS)')
 ax1[1].grid(linestyle='dotted')
 ax1[1].legend(loc = 'upper right')
@@ -135,26 +148,25 @@ ax1[2].set_ylabel(r'$u_S$, $x_S$')
 ax1[2].grid(linestyle='dotted')
 ax1[2].legend(loc = 'upper right')
 
-fig2, ax2 = plt.subplots(nrows=2, ncols=1, sharex=True, gridspec_kw={'height_ratios': [3, 1]},
+fig2, ax2 = plt.subplots(nrows=4, ncols=1, sharex=True, gridspec_kw={'height_ratios': [2,0.6,0.6,1]},
                          num=f'Raster plot, Ne:{N_e} Ni:{N_i}, Iex={I_ex/pA}', figsize=(8,10))
 
-ax2[0].scatter(spikes_exc_mon.t[:]/ms, spikes_exc_mon.i[:], color='C3', marker='|')
-ax2[0].scatter(spikes_inh_mon.t[:]/ms, spikes_inh_mon.i[:]+N_e, color='C0', marker='|')
+ax2[0].scatter(spikes_exc_mon.t[:]/second, spikes_exc_mon.i[:], color='C3', marker='|')
+ax2[0].scatter(spikes_inh_mon.t[:]/second, spikes_inh_mon.i[:]+N_e, color='C0', marker='|')
 ax2[0].set_ylabel('neuron index')
 ax2[0].set_title('Raster plot')
 
-hist_step = 1
-bin_size = (duration/ms)/((duration/ms)//hist_step)*ms
-spk_count, bin_edges = np.histogram(np.r_[spikes_exc_mon.t/ms,spikes_inh_mon.t/ms], 
-                                    int(duration/ms)//hist_step)
-# POPULATION ACTIVITY, ISTANTANEUS FIRING RATE
-# numero di spikes emesso in un breve istante di tempo 
-# meiato su tutta la popolazione
-rate = double(spk_count)/(N_e+N_i)/bin_size
-ax2[1].plot(bin_edges[:-1], rate, '-', color='k')
+ax2[1].plot(firing_rate_exc.t[:]/second, fr_exc, color='C3')
+ax2[2].plot(firing_rate_exc.t[:]/second, fr_ihn, color='C0')
 ax2[1].set_ylabel('rate (Hz)')
-ax2[1].set_xlabel('time (ms)')
+ax2[2].set_ylabel('rate (Hz)')
 ax2[1].grid(linestyle='dotted')
+ax2[2].grid(linestyle='dotted')
+
+ax2[3].plot(state_exc_mon.t[:]/second, LFP, color='C5')
+ax2[3].set_ylabel('LFP')
+ax2[3].set_xlabel('time (s)')
+ax2[3].grid(linestyle='dotted')
 
 # connectivity_EIring(exc_syn, inh_syn)
 # connectivity_plot(exc_syn, source='Exc', target='Exc+Inh', color_s='red', color_t='indigo', size=10)
