b75062aaf6b8a2281672611a039555064572b179
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2022-01-13 10:24:24 +0100

    import module costant for EI network

---
+++ AstrocyteNeuron_Interactions/Networks/constant_EI.py
@@ -0,0 +1,23 @@
+"""
+Module of network parameters that could be changes over simulation
+"""
+# TIME CONSTANT
+dt = 0.05 #ms            #0.05ms
+duration = 2.3 #s    #2.3s
+trans_time = 300 #ms          #300ms
+
+# index: excitatory neurron index used to monitor variable
+index = 400
+
+# NETWORK STRUCTURE
+# how much the ext-inh synaptic strength is highr
+#  than ext-exc synaptic strength
+alpha = 1.0 
+
+# Recurrent balance degrees: g = pe/pi, IMPORTANT: I always fix pe
+# g = 5: recurrent balance
+# g < 5: inhibitory prevails
+# g > 5: excitatory prevails 
+g = 0.25   
+p_e = 0.05  
+
