9671b3e85dc03ca4d799a471f912202af7571dd7
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2022-03-09 10:34:16 +0100

    changed par

--- AstrocyteNeuron_Interactions/Networks/constant_EI.py
+++ AstrocyteNeuron_Interactions/Networks/constant_EI.py
@@ -12,12 +12,12 @@ index = 400
 # NETWORK STRUCTURE
 # how much the ext-inh synaptic strength is highr
 #  than ext-exc synaptic strength
-alpha = 1.3
+alpha = 1.0
 
 # Recurrent balance degrees: g = pe/pi, IMPORTANT: I always fix pe
 # g = 5: recurrent balance
 # g < 5: inhibitory prevails
 # g > 5: excitatory prevails 
-g = 5.0   
+g = 0.25   
 p_e = 0.05  
 
