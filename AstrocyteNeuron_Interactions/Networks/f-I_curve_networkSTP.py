1c101fe5d666395849ffaebfd2bc10ec89864bac
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2022-03-25 11:50:44 +0100

    genral form of EI network with plasticity, usefull plots and analysis

--- AstrocyteNeuron_Interactions/Networks/f-I_curve_networkSTP.py
+++ AstrocyteNeuron_Interactions/Networks/f-I_curve_networkSTP.py
@@ -4,15 +4,16 @@ single excitatory and inhibitory neurons. Basically it is the v_in vs nu_S curve
 """
 import argparse
 import matplotlib.pyplot as plt
-from random import randrange
+from scipy import signal
 from brian2 import *
-from Neuro_Astro_network.network_analysis import transient
-import constant_EI as k_EI
+from Module_network import neurons_firing, transient
 from AstrocyteNeuron_Interactions import makedir
+import constant_EI as k_EI
 
 set_device('cpp_standalone', directory=None)  #1% gain 
 
-parser = argparse.ArgumentParser(description='EI network with costantexternal input (Poisson)')
+parser = argparse.ArgumentParser(description="""EI network with costant external input (Poisson)
+                                                you find the constants in 'constant_EI.py' module""")
 parser.add_argument('r', type=float, help="rate input of external poisson proces")
 parser.add_argument('-p', action='store_true', help="show paramount plots, default=False")
 args = parser.parse_args()
@@ -73,7 +74,7 @@ inh_neurons = neurons[N_e:]
 #External input
 rate_in = args.r*Hz
 poisson = PoissonInput(neurons, 'X_ext', 160 , rate=rate_in, weight='1')
-s = k_EI.alpha
+s = k_EI.s
 exc_neurons.w_ext = w_e
 inh_neurons.w_ext = s*w_e
 
@@ -108,37 +109,122 @@ inh_syn.x_S = 1
 ## RUN and MONITOR  ######################################################################### 
 spikes_exc_mon = SpikeMonitor(exc_neurons)
 spikes_inh_mon = SpikeMonitor(inh_neurons)
+spikes_mon = SpikeMonitor(neurons)
+
 population_fr_exc = PopulationRateMonitor(exc_neurons)
 population_fr_inh = PopulationRateMonitor(inh_neurons)
+population_fr = PopulationRateMonitor(neurons)
 
+state_exc_mon = StateMonitor(exc_neurons, 'LFP', record=True)
 run(duration, report='text')
-
-#Transient time
-trans_time = k_EI.trans_time
-print(f'g = {g}')
-print(f's = {s}')
-pop_exc = population_fr_exc.smooth_rate(window="gaussian", width=0.05*ms)
-pop_inh = population_fr_inh.smooth_rate(window="gaussian", width=0.05*ms)
-print(f'pop-exc: {pop_exc.mean()}')
-print(f'pop-inh: {pop_inh.mean()}')
-
-
+#################################################################################################
+## SAVE VARIABLE ################################################################################
 name = f"Neural_network/EI_net_STP/f-I_curve/Network_pe_v_in{rate_in}_g{g}_s{s}_we{w_e/nS:.2f}_fIcurve"
 makedir.smart_makedir(name)
 
-## SAVE VARIABLE ################################################################################
 np.save(f'{name}/duration',duration)
 np.save(f'{name}/g',g)
 np.save(f'{name}/s',s)
 np.save(f'{name}/rate_in',rate_in)
 
-np.save(f'{name}/pop_exc',pop_exc)
-np.save(f'{name}/pop_inh',pop_inh)
+np.save(f'{name}/fr_exc', population_fr_exc.rate[:])
+np.save(f'{name}/fr_inh', population_fr_exc.rate[:])
+np.save(f'{name}/fr', population_fr.rate[:])
 #################################################################################################
+
+## ANALYSIS #####################################################################################
+#Transient time
+trans_time = k_EI.trans_time
+trans = transient(population_fr.t[:]/second*second, trans_time)
+
+## Spectral analysis and Neuron's firing rates distribuction
+LFP = state_exc_mon.LFP[:].sum(axis=0)
+firing_rate_exc = population_fr_exc.smooth_rate(window="gaussian", width=0.05*ms)
+firing_rate_inh = population_fr_inh.smooth_rate(window="gaussian", width=0.05*ms)
+
+freq_fr_exc, spect_fr_exc = signal.welch(firing_rate_exc[trans:], fs=1/defaultclock.dt, nperseg=len(firing_rate_exc[trans:])//4)
+freq_fr_inh, spect_fr_inh = signal.welch(firing_rate_inh[trans:], fs=1/defaultclock.dt, nperseg=len(firing_rate_inh[trans:])//4)
+freq_fr, spectrum_fr = signal.welch(population_fr.rate[trans:], fs=1/defaultclock.dt, nperseg=len(population_fr.rate[trans:])//4)
+freq_LFP, spectrum_LFP = signal.welch(LFP[trans:], fs=1/defaultclock.dt/Hz, nperseg=len(LFP[trans:])//4)
+
+neurons_fr = neurons_firing(spikes_mon.t[:]/second, spikes_mon.i[:], time_start=0.5, time_stop=2.3)
+exc_neurons_fr = neurons_firing(spikes_exc_mon.t[:]/second, spikes_exc_mon.i[:], time_start=0.5, time_stop=2.3)
+inh_neurons_fr = neurons_firing(spikes_inh_mon.t[:]/second, spikes_inh_mon.i[:], time_start=0.5, time_stop=2.3)
+
+## some information
+print('NETWORK')
+print(f'transient time = {trans_time} ms')
+print(f'g = {g}')
+print(f's = {s}')
+print(f'pop-exc: {firing_rate_exc[trans:].mean()}')
+print(f'pop-inh: {firing_rate_inh[trans:].mean()}')
+
+plt.figure(num='Firing rate distribuction')
+plt.hist(neurons_fr/Hz, bins=13)
+plt.hist(exc_neurons_fr/Hz, bins=13, label='Exc')
+plt.hist(inh_neurons_fr/Hz, bins=13, label='Inh')
+plt.legend()
+
 #################################################################################################
-plt.figure()
-plt.scatter(spikes_exc_mon.t[:], spikes_exc_mon.i[:], marker='|', color='C3')
-plt.scatter(spikes_inh_mon.t[:], spikes_inh_mon.i[:]+N_e, marker='|', color='C0')
+if args.p:
+    fig1, ax1 = plt.subplots(nrows=4, ncols=1, sharex=True, gridspec_kw={'height_ratios': [2,0.6,0.6,1]},
+								num=f'Raster plot, v_in={rate_in/Hz} (no STP)', figsize=(8,10))
+
+    ax1[0].scatter(spikes_exc_mon.t[:]/second, spikes_exc_mon.i[:], color='C3', marker='|')
+    ax1[0].scatter(spikes_inh_mon.t[:]/second, spikes_inh_mon.i[:]+N_e, color='C0', marker='|')
+    ax1[0].set_ylabel('neuron index')
+    ax1[0].set_xlim([0.3,2.3])
+    ax1[0].set_title('Raster plot')
+
+    ax1[1].plot(population_fr_exc.t[trans:]/second, firing_rate_exc[trans:], color='C3')
+    ax1[2].plot(population_fr_inh.t[trans:]/second, firing_rate_inh[trans:], color='C0')
+    ax1[1].set_ylabel('rate (Hz)')
+    ax1[2].set_ylabel('rate (Hz)')
+    ax1[1].set_xlim([0.25,2.4])
+    ax1[2].set_xlim([0.25,2.4])
+    ax1[1].grid(linestyle='dotted')
+    ax1[2].grid(linestyle='dotted')
+
+    ax1[3].plot(state_exc_mon.t[trans:]/second, LFP[trans:], color='C5')
+    ax1[3].set_ylabel('LFP (mV)')
+    ax1[3].set_xlabel('time (s)')
+    ax1[3].set_xlim([0.25,2.4])
+    ax1[3].grid(linestyle='dotted')
+
+    plt.savefig(name+f'/Raster plot, v_in={rate_in/Hz} (no STP).png')
+
+    fig2, ax2 = plt.subplots(nrows=1, ncols=1, figsize=(7,6),
+								num=f"Neuron's firing rate distribuction - {rate_in/Hz}")
+
+    ax2.hist(neurons_fr/Hz, bins=12, color='k', alpha=0.5, label='Total population' )
+    ax2.hist(exc_neurons_fr/Hz, bins=6, color='C3', label='Exc', alpha=0.65 )
+    ax2.hist(inh_neurons_fr/Hz, bins=6, color='C0', label='Inh', alpha=0.65)
+    ax2.set_xlabel('frequency (Hz)')
+    ax2.set_ylabel("Number of neurons")
+    ax2.legend()
+
+    plt.savefig(name+f"/Neuron's firing rate distribuction - {rate_in/Hz}.png")
+
+
+    fig3, ax3 = plt.subplots(nrows=1, ncols=2, figsize=(12,6),
+								num=f"Power Spectrum - {rate_in/Hz}")
+
+    ax3[0].set_title('Population Firing Rate')
+    ax3[0].plot(freq_fr, spectrum_fr, color='k')
+    ax3[0].set_xlabel('frequency (Hz)')
+    ax3[0].grid(linestyle='dotted')
+    
+    ax3[1].set_title('LFP')
+    ax3[1].plot(freq_LFP, spectrum_LFP, color='C5')
+    ax3[1].plot(freq_LFP, 1/(freq_LFP**2), lw=0.7, alpha=0.9)
+    ax3[1].set_xscale('log')
+    ax3[1].set_yscale('log')
+    ax3[1].set_xlabel('frequency (Hz)')
+    ax3[1].grid(linestyle='dotted')
+    plt.savefig(name+f"/Power Spectrum - {rate_in/Hz}.png")
+
+
+    device.delete()
+    plt.show()
 
 device.delete()
-# plt.show()
